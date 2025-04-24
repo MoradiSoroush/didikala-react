409fd278a2f88e8542aa450416c61d9e535d5408
soroush <[smbinance2020@gmail.com]>
2025-04-24 13:13:46 +0330

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React App
+          learn it very fast
+          
         </a>
       </header>
     </div>
